20b260dd9573380393ffb229e327a55c1bba42cc
avto.jaja <52544731+avtojaja@users.noreply.github.com>
2024-11-02 20:43:38 +0400

    Add week-3 code snippets.

---
+++ week-3/3_func_args.py
@@ -0,0 +1,16 @@
+def my_function(fname, lname):
+    print(fname + " " + lname)
+
+my_function("Emil", "Refsnes")
+
+
+# def my_function(fname, lname):
+#     print(fname + " " + lname)
+
+# my_function("Emil")
+
+
+# def my_function(fname, lname):
+#     print(fname + " " + lname)
+
+# my_function("Emil", "Emil", "Emil")
---
+++ week-3/4_func_args_n.py
@@ -0,0 +1,5 @@
+def my_function(*kids):
+    # print("The youngest child is " + kids[2])
+    print(kids)
+
+my_function("Emil", "Tobias", "Linus")
